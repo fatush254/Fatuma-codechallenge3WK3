e58686b7111e521f724e3e9b4f46b7b58a04e6c2
fatush254 <fgalgalo945@gmail.com>
2024-04-08 08:49:14 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // Your code here
-let filmEndpoint= 'http://localhost:4000/films';
+
+
+let filmEndpoint = "http://localhost:4000/films";
 
 document.addEventListener("DOMContentLoaded", () => {
     getMovies();
@@ -8,15 +10,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function getMovies() {
     fetch(filmEndpoint)
-    .then(res => res.json())
-    .then(movies => {
-        movies.forEach(movie => {renderMovieList(movie)})
-        const firstMovie = document.querySelector("#id1");
-        firstMovie.dispatchEvent(new Event("click"));
-    })
+        .then(res => res.json())
+        .then(movies => {
+            movies.forEach(movie => {
+                renderMovieList(movie);
+            });
+            const firstMovie = document.querySelector("#id1");
+            firstMovie.dispatchEvent(new Event("click"));
+        });
 }
 
-console.log(getMovies);
+
 
 function renderMovieList(movie) {
     const li = document.createElement("li");
@@ -25,23 +29,37 @@ function renderMovieList(movie) {
     const ul = document.querySelector("#films");
     ul.appendChild(li);
     li.classList.add("film");
-    li.classList.add('item');
-    li.addEventListener("click", () => {handleMovieClick(movie)})
-}
-console.log(renderMovieList);
+    li.classList.add("item");
 
-function handleMovieClick(movie ){
-    const poster = document.querySelector("img#poster")
+    // Add delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+        removeFilm(movie.id);
+    });
+    li.appendChild(deleteButton);
+
+    li.addEventListener("click", () => {
+        handleMovieClick(movie);
+    });
+
+
+}
+
+
+function handleMovieClick(movie) {
+    const poster = document.querySelector("img#poster");
     poster.src = movie.poster;
     poster.alt = movie.title;
     const info = document.querySelector("#showing");
     info.querySelector("#title").textContent = movie.title;
-    info.querySelector("#runtime").textContent = movie.runtime+" minutes";
+    info.querySelector("#runtime").textContent = movie.runtime + " minutes";
     info.querySelector("#film-info").textContent = movie.description;
     info.querySelector("#showtime").textContent = movie.showtime;
-    info.querySelector("#ticket-num").textContent = movie.capacity - movie.tickets_sold + " remaining tickets";
+    info.querySelector("#ticket-num").textContent =
+        movie.capacity - movie.tickets_sold + " remaining tickets";
 }
-console.log(handleMovieClick);
+
 
 function handleBuyTicket(Ticket) {
     const ticketDiv = document.querySelector("#ticket-num");
@@ -49,10 +67,24 @@ function handleBuyTicket(Ticket) {
     if (tickets > 0) {
         ticketDiv.textContent = tickets - 1 + " remaining tickets";
     }
-    else if (tickets == 0) {
-        alert("No more tickets!");
-        e.target.classList.add("sold-out");
-        e.target.classList.remove("orange");
-    }
+    
 }
-console.log(handleBuyTicket);
+
+
+function removeFilm(filmId) {
+    // Remove film from the list (client-side)
+    const filmElement = document.querySelector(`#id${filmId}`);
+    filmElement.remove();
+
+    // Delete film on the server
+    fetch(`${filmEndpoint}/${filmId}`, {
+        method: "DELETE",
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(`Film with ID ${filmId} deleted on the server.`);
+        })
+        .catch(error => {
+            console.error(`Error deleting film with ID ${filmId}: ${error.message}`);
+        });
+}
