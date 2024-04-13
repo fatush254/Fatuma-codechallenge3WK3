cd30a7e9908de40b351621dfc1081ec22bd397b9
fatush254 <fgalgalo945@gmail.com>
2024-04-13 12:29:32 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,90 +1,108 @@
-// Your code here
+// start a new GET request to get the server running on the backend
+const myGetRequest = new Request('http://localhost:4000/films');
+// Get all IDs in the HTML for DOM manipulation
+const title = document.getElementById('title');
+const runtime = document.getElementById('runtime');
+const filmInfo = document.getElementById('film-info');
+const showtime = document.getElementById('showtime');
+const ticketNum = document.getElementById('ticket-num');
+const buyTicket = document.getElementById('buy-ticket');
+const poster = document.getElementById('poster');
+const films = document.getElementById('films');
+const subtitle = document.getElementById('subtitle');
+const showing = document.getElementById('showing');
+const body = document.getElementsByTagName('body')[0]; // Get the first body element
 
+//start function
+// On full load, the first movie should be displayed as default
+window.onload = () => {
+	// fetch details from the server
+	fetch(myGetRequest)
+		//JSONIFY the response
+		.then((response) => response.json())
+		//.then to handle async and promise from the server
+		.then((data) => {
+			// first movie should start from the first data in the array
+			const firstMovie = data[0];
+			// get the number of remaining tickets to sell from subtracting capacity - tickets_sold
+			let remainingTickets = firstMovie.capacity - firstMovie.tickets_sold;
+			// manipulate the HTML from the JavaScript side by targeting the IDs
 
-let filmEndpoint = "http://localhost:4000/films";
-
-document.addEventListener("DOMContentLoaded", () => {
-    getMovies();
-    document.querySelector("#buy-ticket").addEventListener("click", handleBuyTicket);
-});
-
-function getMovies() {
-    fetch(filmEndpoint)
-        .then(res => res.json())
-        .then(movies => {
-            movies.forEach(movie => {
-                renderMovieList(movie);
-            });
-            const firstMovie = document.querySelector("#id1");
-            firstMovie.dispatchEvent(new Event("click"));
-        });
-}
-
-
-
-function renderMovieList(movie) {
-    const li = document.createElement("li");
-    li.textContent = `${movie.title}`;
-    li.id = "id" + movie.id;
-    const ul = document.querySelector("#films");
-    ul.appendChild(li);
-    li.classList.add("film");
-    li.classList.add("item");
-
-    // Add delete button
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.addEventListener("click", () => {
-        removeFilm(movie.id);
-    });
-    li.appendChild(deleteButton);
-
-    li.addEventListener("click", () => {
-        handleMovieClick(movie);
-    });
-
-
-}
-
-
-function handleMovieClick(movie) {
-    const poster = document.querySelector("img#poster");
-    poster.src = movie.poster;
-    poster.alt = movie.title;
-    const info = document.querySelector("#showing");
-    info.querySelector("#title").textContent = movie.title;
-    info.querySelector("#runtime").textContent = movie.runtime + " minutes";
-    info.querySelector("#film-info").textContent = movie.description;
-    info.querySelector("#showtime").textContent = movie.showtime;
-    info.querySelector("#ticket-num").textContent =
-        movie.capacity - movie.tickets_sold + " remaining tickets";
-}
-
-
-function handleBuyTicket(Ticket) {
-    const ticketDiv = document.querySelector("#ticket-num");
-    const tickets = ticketDiv.textContent.split(" ")[0];
-    if (tickets > 0) {
-        ticketDiv.textContent = tickets - 1 + " remaining tickets";
-    }
+			title.innerHTML = `${firstMovie.title}`;
+			runtime.innerHTML = `${firstMovie.runtime}`;
+			filmInfo.innerHTML = `${firstMovie.description}`;
+			showtime.innerHTML = `${firstMovie.showtime}`;
+			ticketNum.innerHTML = `${remainingTickets}`;
+			buyTicket.innerHTML = 'Buy ticket';
+			poster.src = `${firstMovie.poster};`
+			// eventlistener for clicking the buy ticket button
+			buyTicket.addEventListener('click', () => {
+				// check if the remaining tickets are more than 0, then start subtracting
+				if (remainingTickets > 0) {
+					// -- to deduct
+					remainingTickets--;
+					// display dynamically on the html
+					ticketNum.innerHTML = `${remainingTickets}`;
+				} else if (remainingTickets === 0) {
+					// once tickets remaining are 0, show a sold out on the button and disable buying more tickets
+					ticketNum.innerHTML = `${remainingTickets}`;
+					buyTicket.innerHTML = `Sold out!`;
+				}
+			});
+			// remove the first movie so as to proceed adding the rest
+			films.innerHTML = '';
+			// forEach to add the movies one by  one, just like a for loop
+			data.forEach((movie, index) => {
+				// create a list to display all the movies
+				const li = document.createElement('li');
+				// dynamically show the movie titles
+				li.innerHTML = <b>${movie.title}</b>;
+				// append the new list to the films ID on the HTML
+				films.appendChild(li);
+				// add a button to delete each movie
+				const deleteButton = document.createElement('button');
+				deleteButton.innerHTML = 'Delete';
+				// add class using JS
+				deleteButton.classList.add('ui', 'button');
+				// add space/style using css but on JS
+				deleteButton.style.marginLeft = '5px';
+				// append the new button on the HTML
+				li.appendChild(deleteButton);
+				// add cursor/color when one hovers over a movie, will use eventlistener for mouseover
+				li.addEventListener('mouseover', () => {
+					li.style.color = 'red';
+					li.style.cursor = 'pointer';
+				});
+				// when one isn't hovering the movie name, return to default color
+				li.addEventListener('mouseout', () => {
+					li.style.color = 'black';
+				});
+				// delete button for each movie
+				//only delete when user confirms/selects okay
+				deleteButton.addEventListener('click', () => {
+					// alert for user to confirm
+					if (window.confirm('Are you sure you want to delete this movie?')) {
+						// perform the deletion
+						data.splice(index, 1);
+						// Update the list and remove the movie
+						films.removeChild(li);
+					}
+				});
+				// when a user selects a certain movie, update the rest of the data e.g description accordingly and dynamically
+				li.addEventListener('click', () => {
+					remainingTickets = movie.capacity - movie.tickets_sold;
+					title.innerHTML = `${movie.title}`;
+					runtime.innerHTML = `${movie.runtime}`;
+					filmInfo.innerHTML = `${movie.description}`;
+					showtime.innerHTML = `${movie.showtime}`;
+					ticketNum.innerHTML = `${remainingTickets}`;
+					buyTicket.innerHTML = 'Buy ticket';
+					poster.src = `${movie.poster}`;
+				});
+			});
+		});
+};
     
-}
 
 
-function removeFilm(filmId) {
-    // Remove film from the list (client-side)
-    const filmElement = document.querySelector(`#id${filmId}`);
-    filmElement.remove();
 
-    // Delete film on the server
-    fetch(`${filmEndpoint}/${filmId}`, {
-        method: "DELETE",
-    })
-        .then(res => res.json())
-        .then(data => {
-            console.log(`Film with ID ${filmId} deleted on the server.`);
-        })
-        .catch(error => {
-            console.error(`Error deleting film with ID ${filmId}: ${error.message}`);
-        });
-}
